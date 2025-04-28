21843cbd77f4dfd4df8229162bd99503333ea199
23046ys <23046ys@hvhs.school.nz>
2025-04-28 22:00:25 +0000

    Set up template

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,20 @@
-
-
+/****************************
+ Task-01 Intro to Js
+  Yuv Singh
+  29/04/2025
+The purpose of this is to be a template for all the pages
+ ***************************/
 
 console.log("Running t01_introduction.js");
+
+//Variables
+
+
+/****************************
+Main Code:
+ ***************************/
+
+
+/****************************
+Functions
+ ***************************/
