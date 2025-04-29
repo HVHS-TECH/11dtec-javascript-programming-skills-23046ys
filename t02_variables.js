16e4fa3c3080f6d58df20a60a51ffd7808af3036
23046ys <23046ys@hvhs.school.nz>
2025-04-29 23:48:20 +0000

    Apr 29, 2025, 11:48 PM

---
+++ t02_variables.js
@@ -0,0 +1,21 @@
+/****************************
+ Task-02 Variables
+  Yuv Singh
+  30/04/2025
+The purpose of this is to 
+ ***************************/
+
+console.log("Running t02_variables.js");
+console.log("H");
+
+//Variables
+
+
+/****************************
+Main Code:
+ ***************************/
+
+
+/****************************
+Functions
+ ***************************/
