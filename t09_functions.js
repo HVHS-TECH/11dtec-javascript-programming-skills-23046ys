44b6637cef6a40b4d9f36eb5b8d6e83bde6dcd52
23046ys <23046ys@hvhs.school.nz>
2025-05-08 20:55:46 +0000

    May 8, 2025, 8:55 PM

---
+++ t09_functions.js
@@ -0,0 +1,28 @@
+/****************************
+ Task-09 The for loop
+  Yuv Singh
+  09/05/2025
+The purpose of this is 
+ ***************************/
+
+console.log("Running t09_functions.js");
+console.log("H");
+
+//Variables
+var UserNumber
+var total= 0;
+for (count = 0; count<5; count++) {
+    UserNumber= Number(prompt("Pick a Number"))
+    total= total + UserNumber;
+}
+console.log("Total: " + total)
+alert("The total of the five numbers you picked is " + total)
+
+/****************************
+Main Code:
+ ***************************/
+
+
+/****************************
+Functions
+ ***************************/
