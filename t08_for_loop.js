bd4739a00e232bae9303406a69cdd5624072037f
23046ys <23046ys@hvhs.school.nz>
2025-05-08 20:48:54 +0000

    May 8, 2025, 8:48 PM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -9,10 +9,10 @@ console.log("Running t08_for_loop.js");
 console.log("H");
 
 //Variables
-
+var UserNumber
 var total= 0;
 for (count = 0; count<5; count++) {
-    prompt()
+    
     total= total + count;
 }
 console.log("Total: " + total)
