eb3d05211af5925e6ef8aae61c2a4c1e6b5f6b0f
23046ys <23046ys@hvhs.school.nz>
2025-06-05 01:30:27 +0000

    gg

---
+++ t17_testing_boundary_invalid.js
@@ -0,0 +1,145 @@
+/****************************
+ Task-17 Testing Boundary Invalid
+  Yuv Singh
+  09/05/2025
+The purpose of this is 
+ ***************************/
+
+
+
+console.log("Running t17_testing_boundary_invalid.js");
+console.log("H");
+
+//Variables
+
+
+var Name
+var PocketMoney
+var ChocolateAffordability=0;
+var Age
+var ValidName
+var ValidAge
+var ValidPocket
+let chocArray = ["Mars Bar", "Moro Bar", "King Size Crunchy", "Dairy Milk Block", "Lindt Block"]
+let priceArray = [2,2.50,3,6,7]
+var gfhGh
+/****************************
+Main Code:
+ ***************************/
+
+/****************************
+Functions
+ ***************************/
+function askUserName() {
+Name= prompt('Welcome to my program ' + Name + '!\nPlease enter your name', Name);
+if(Name == null){
+ return;
+}
+if (!isNaN(Name) || Name == "" || Name == " "){
+ValidName= "Invalid";
+while( ValidName == "Invalid"){
+  Name= prompt('Invalid Name');
+  if(Name == null){
+ return;
+}
+if (!isNaN(Name) || Name == "" || Name == " "){
+  alert("Invalid Name")
+} else {
+  ValidName= "Valid"
+}
+}
+}
+}
+function askUserAge() {
+Age= prompt('Please enter your age', Age)
+if(Age == null){
+ return;
+}
+if (isNaN(Age) || Age <= 0 || Age > 100 || Age == "" ){
+ValidAge= "Invalid";
+while( ValidAge == "Invalid"){
+  Age= prompt('Invalid Age');
+  if(Age == null){
+ return;
+}
+if (isNaN(Age) || Age <= 0 || Age > 100 || Age == "" ){
+  alert("Invalid Age")
+} else {
+  ValidAge= "Valid"
+}
+}
+}
+}
+function askUserMoney() {
+  
+  PocketMoney= prompt('How much money do you have from 1-10?', PocketMoney)
+  if(PocketMoney == null){
+ return;
+}
+ 
+  if(isNaN(PocketMoney) || PocketMoney < 0 || PocketMoney > 10 || PocketMoney == "" ){
+   ValidPocket= "Invalid";
+    while(ValidPocket == "Invalid"){
+          
+      PocketMoney= prompt('Invalid Number', PocketMoney)
+      if(PocketMoney == null){
+ return;
+}
+if(isNaN(PocketMoney) || PocketMoney < 0 || PocketMoney > 10 || PocketMoney == "" ){
+alert("Invalid Pocket Money")
+}else{
+  ValidPocket="Valid"
+  if(PocketMoney == null){
+    return;
+  }
+}
+
+
+    }
+  } 
+  
+if(PocketMoney >= priceArray[5]){
+ChocolateAffordability= 5;
+console.log(ChocolateAffordability)
+}else if(PocketMoney >= priceArray[4]){
+ChocolateAffordability= 4;
+console.log(ChocolateAffordability)
+}else if(PocketMoney >= priceArray[3]){
+ChocolateAffordability= 3;
+console.log(ChocolateAffordability)
+}else if(PocketMoney >= priceArray[2]){
+ChocolateAffordability= 2;
+console.log(ChocolateAffordability)
+}else if(PocketMoney >= priceArray[1]){
+ChocolateAffordability= 1;
+console.log(ChocolateAffordability)
+}else{
+  ChocolateAffordability= 0;
+  console.log(ChocolateAffordability)
+}
+if(ChocolateAffordability > 0){
+  gfhGh= "You can afford a " + chocArray[ChocolateAffordability] + " for the price of $" + priceArray[ChocolateAffordability];
+  console.log("Afford")
+}else{
+  gfhGh= "You can't afford anything.";
+  console.log("Not afford")
+}
+document.getElementById("chocText").innerHTML= gfhGh;
+}
+
+
+
+function programStart() {
+    askUserName();
+     if(Name == null){
+ return;
+}
+    askUserAge();
+     if(Age == null){
+ return;
+}
+    askUserMoney();
+     if(PocketMoney == null){
+ return;
+}
+}
